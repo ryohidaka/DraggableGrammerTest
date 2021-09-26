be347846cf523e7c2712f9ddd38b82aa4b170a2a
ryohidaka <git@ryohidaka.jp>
2021-09-26 18:13:56 +0900

    feat: テスト画面を追加

---
+++ src/views/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">
+    <p>Q</p>
+    <p>{{ test.question }}</p>
+
+    <p>A</p>
+    <p v-for="word in test.answer" :key="word">{{ word }}</p>
+  </div>
+</template>
+
+<script>
+import store from "../store";
+export default {
+  name: "Test",
+  computed: {
+    id() {
+      return this.$route.params["id"];
+    },
+    test() {
+      return store.getters["test"](this.id);
+    },
+  },
+};
+</script>
