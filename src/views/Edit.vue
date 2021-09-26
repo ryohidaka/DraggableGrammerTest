9b0818a053543143a8ce5f0a45e78b3ceb4a92b1
ryohidaka <git@ryohidaka.jp>
2021-09-26 16:57:29 +0900

    feat: 入力フォームを作成

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="edit">
     <h1>問題一覧</h1>
-    <v-list two-line>
-      <v-list-item v-for="test in tests" :key="test.question">
-        <v-list-item-content>
-          <v-list-item-subtitle v-text="test.question"></v-list-item-subtitle>
-          <v-list-item-title>
-            <span
-              v-for="(word, index) in test.answer"
-              :key="index"
-              v-text="word"
-            />
-          </v-list-item-title>
-        </v-list-item-content>
+    <v-simple-table>
+      <template v-slot:default>
+        <tbody>
+          <tr v-for="test in tests" :key="test.question">
+            <v-list two-line>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title v-text="test.question"></v-list-item-title>
+                  <v-list-item-title>
+                    <span
+                      v-for="(word, index) in test.answer"
+                      :key="index"
+                      v-text="word"
+                    />
+                  </v-list-item-title>
+                </v-list-item-content>
 
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-information</v-icon>
-          </v-btn>
-        </v-list-item-action>
-        <v-divider inset></v-divider>
-      </v-list-item>
-    </v-list>
+                <v-list-item-action>
+                  <v-btn icon>
+                    <v-icon color="grey lighten-1">mdi-information</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
 
     <editor />
   </div>
