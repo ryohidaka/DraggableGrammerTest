549b8f22395d3bc681048552a8cd82d316946115
ryohidaka <git@ryohidaka.jp>
2021-09-26 18:37:30 +0900

    feat: 解答と採点機能を追加

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -4,20 +4,42 @@
     <p>{{ test.question }}</p>
 
     <p>A</p>
-    <p v-for="word in test.answer" :key="word">{{ word }}</p>
+    <draggable v-model="items">
+      <span v-for="word in items" :key="word">{{ word }}</span>
+    </draggable>
+
+    <button @click="submit">回答</button>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 import store from "../store";
+
 export default {
   name: "Test",
+  components: { draggable },
+  data: () => ({
+    items: [],
+  }),
+  mounted() {
+    this.items = this.test.answer;
+  },
   computed: {
     id() {
       return this.$route.params["id"];
     },
     test() {
       return store.getters["test"](this.id);
+    },
+  },
+  methods: {
+    submit() {
+      if (this.test.answer === this.items) {
+        alert("正解");
+      } else {
+        alert("不正解");
+      }
     },
   },
 };
