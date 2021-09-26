bec5b919b7664cb1336a42600c596eea94bfcd62
ryohidaka <git@ryohidaka.jp>
2021-09-26 17:00:16 +0900

    feat: データを永続化するように変更

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -19,4 +20,5 @@ export default new Vuex.Store({
   },
   actions: {},
   modules: {},
+  plugins: [createPersistedState()],
 });
