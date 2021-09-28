d82124a44475e56914e419ab3a355c7130973a7c
ryohidaka <git@ryohidaka.jp>
2021-09-28 23:18:51 +0900

    feat: テスト画面のデザインを修正

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,30 +1,43 @@
 <template>
-  <v-container>
-    <v-col cols="12" md="10">
-      <v-row>
+  <v-row class="test" justify="center" align-content="center">
+    <v-col cols="12" md="6">
+      <v-col>
         <h2>
           <v-icon>mdi-chat-question-outline</v-icon>
           {{ test.question }}
         </h2>
-      </v-row>
+      </v-col>
 
       <v-col>
         <v-icon>mdi-alpha-a-circle-outline</v-icon>
       </v-col>
-      <v-col align="center" justify="center">
-        <v-chip-group>
-          <draggable v-model="items">
-            <v-chip v-for="word in items" :key="word" x-large>
-              {{ word }}
-            </v-chip>
+
+      <v-col>
+        <v-row justify="center">
+          <draggable
+            v-model="items"
+            class="draggable"
+            :options="{ animation: 300 }"
+          >
+            <v-col v-for="word in items" :key="word">
+              <v-card height="100" width="100" dark class="primary">
+                <v-row
+                  class="fill-height text-h4"
+                  align="center"
+                  justify="center"
+                  v-text="word"
+                />
+              </v-card>
+            </v-col>
           </draggable>
-        </v-chip-group>
+        </v-row>
       </v-col>
-      <v-col align="center" justify="center">
+
+      <v-col align="center">
         <v-btn x-large color="success" @click="submit"> 回答 </v-btn>
       </v-col>
     </v-col>
-  </v-container>
+  </v-row>
 </template>
 
 <script>
@@ -59,3 +72,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.test {
+  height: 100%;
+}
+.draggable {
+  display: flex;
+}
+</style>
